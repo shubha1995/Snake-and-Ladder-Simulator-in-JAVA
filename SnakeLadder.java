99a246a2d2553d86df83060ae9b557d68d28dc63
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-16 20:57:15 +0530

    Added: Player rolls the die to get number between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,10 @@ public class SnakeLadder{
 	int Position = 0;
 	System.out.println("Welcome to Snake and Ladder Simulator");
 	System.out.println("Your Starting Position is : " +Position);
+	Dice();
 	}
+	public static void Dice(){
+		int Dice = (int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Your Dice Value is : " +Dice);
    }	
+}
