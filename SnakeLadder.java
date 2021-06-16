997c7f3304c7671ec1c8bf3303cef83a4145cc53
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-17 01:27:39 +0530

    Added: Repeat till the player reached 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class SnakeLadder{
 public static void main(String[] args){
-	int position = 0;
-	System.out.println("position is " + position);
-	int dice = (int) (Math.random() * 10) % 6+1;
-	System.out.println("dice value is " + dice);
 	
+  int position = 0;
+  while(position<=99){
+	System.out.println("position is " + position);
+	int dice=(int) (Math.random() * 10) % 6+1;
+		System.out.println("dice value is " + dice);
 	int check = (int) (Math.random() * 10) % 3+1;
-	System.out.println("case number is " + check);
+	
 	 switch(check) {
 	   case 1:
 	   System.out.println( "No Play You are in the same position" );
@@ -16,6 +17,10 @@ public static void main(String[] args){
 	  
 	   case 2:
 	   position = (position + dice);
+	   if(position>100){
+		   position = (position - dice);
+		   continue;
+	   }
 	   System.out.println( "your position after ladder is :" + position );
 	   break;
 	   
@@ -30,3 +35,4 @@ public static void main(String[] args){
 	   }
 	  }
 	 }
+	}
