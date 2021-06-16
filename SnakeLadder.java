9028684f57759805ff18eecb5991a886757b8f5b
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-16 20:49:59 +0530

    Added: Starting position of Dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,7 @@
 public class SnakeLadder{
 	public static void main(String[] args){
-	
+	int Position = 0;
 	System.out.println("Welcome to Snake and Ladder Simulator");
+	System.out.println("Your Starting Position is : " +Position);
 	}
    }	
