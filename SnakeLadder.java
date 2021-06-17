564e0fd1b37870b0289b080fb82a0f430767c771
shubhasankar <60878225+shubha1995@users.noreply.github.com>
2021-06-17 07:44:25 +0530

    Update SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,7 +7,8 @@ public static void main(String[] args){
   while(position<=99){
 	System.out.println("position is " + position);
 	int dice=(int) (Math.random() * 10) % 6+1;
-		System.out.println("dice value is " + dice);
+	position = (position - dice);
+	System.out.println("dice value is " + dice);
 	int check = (int) (Math.random() * 10) % 3+1;
 	
 	 switch(check) {
@@ -17,22 +18,18 @@ public static void main(String[] args){
 	  
 	   case 2:
 	   position = (position + dice);
-	   if(position>100){
-		   position = (position - dice);
-		   continue;
-	   }
 	   System.out.println( "your position after ladder is :" + position );
 	   break;
 	   
 	   case 3: 
 	   position = (position - dice);
 	  
-	  if(position<0){
+	   if(position<0){
 		  position = 0;
-	  }
+	   }
 		 System.out.println( "Your Position after snake is" + position );
 	   break;
-	   }
 	  }
 	 }
-	}
+       }
+     }
